ce6d0751a04831e6e0009dad3a18c9717bcce640
Shahar Vardi <v4rd1@protonmail.com>
2021-01-19 16:06:09 +0200

    Update multiPing.ps1

--- multiPing.ps1
+++ multiPing.ps1
@@ -5,5 +5,5 @@ while($i -lt $h){Read-Host "Enter IP" >> .\ips.txt -ErrorAction Ignore; $i+=1}
 foreach($ip in $ips){
 if(Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue){
 Write-Host "$ip is Online!"; $ips = Write-Host $null > .\ips.txt}
-else{Write-Host "$ip is Offline!"; $ips = Write-Host $null > ,\ips.txt}
+else{Write-Host "$ip is Offline!"; $ips = Write-Host $null > .\ips.txt}
 }
