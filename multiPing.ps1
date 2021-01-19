2335ed87df5149a996501b173de314b14be04edc
Shahar Vardi <v4rd1@protonmail.com>
2021-01-19 16:05:51 +0200

    Create multiPing.ps1

---
+++ multiPing.ps1
@@ -0,0 +1,9 @@
+$i = 0
+$h = Read-Host "How many hosts to ping?"
+$ips = Get-Content .\ips.txt
+while($i -lt $h){Read-Host "Enter IP" >> .\ips.txt -ErrorAction Ignore; $i+=1}
+foreach($ip in $ips){
+if(Test-Connection -ComputerName $ip -Count 1 -ErrorAction SilentlyContinue){
+Write-Host "$ip is Online!"; $ips = Write-Host $null > .\ips.txt}
+else{Write-Host "$ip is Offline!"; $ips = Write-Host $null > ,\ips.txt}
+}
